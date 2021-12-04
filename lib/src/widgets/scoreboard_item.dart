82d9e930264ca07b3386018772905cec51a82e46
Samuel Jackson <samuel.jackson@workiva.com>
2021-12-04 12:09:42 -0600

    Stub-out UI for made/broke frames

--- lib/src/widgets/scoreboard_item.dart
+++ lib/src/widgets/scoreboard_item.dart
@@ -6,7 +6,39 @@ import 'package:flutter/material.dart';
 class ScoreboardItem extends StatelessWidget {
   final ScoreboardEntry entry;
 
+  int get bid => entry.bid ?? 0;
+  int? get tricks => entry.tricks;
+
+  bool get broke => tricks != null && bid != tricks;
+
   const ScoreboardItem(this.entry, {Key? key}) : super(key: key);
+
+  Widget _renderBidAndTricks() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        if (broke)
+          Padding(
+            padding: const EdgeInsets.only(right: 1.5),
+            child: Text(
+              '$tricks',
+              style: const TextStyle(fontSize: 12.0, color: Colors.red),
+            ),
+          ),
+        Padding(
+          padding: const EdgeInsets.only(right: 1.5),
+          child: Text(
+            '$bid',
+            style: TextStyle(
+              fontSize: 12.0,
+              decoration: broke ? TextDecoration.lineThrough : null,
+              color: broke ? null : Colors.green,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 
   Widget buildStack() {
     return SizedBox(
@@ -19,25 +51,22 @@ class ScoreboardItem extends StatelessWidget {
                 textDirection: TextDirection.ltr,
                 end: 0,
                 top: 0,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 1.5),
-                      child: Text(
-                        '${entry.bid ?? 0}',
-                        style: const TextStyle(fontSize: 12.0),
-                      ),
-                    ),
-                  ],
-                ),
+                child: _renderBidAndTricks(),
               ),
               Positioned.fill(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text('${entry.score}'),
+                    if (broke)
+                      Container(
+                          padding: const EdgeInsets.all(2.0),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Colors.red),
+                          ),
+                          child: Text('${entry.score}')),
+                    if (!broke) Text('${entry.score}'),
                   ],
                 ),
               ),
